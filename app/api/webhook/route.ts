3f02ed56dc6a91691efe992dcd17750a1274b983
Estifanos Wendimu <teffsauce@gmail.com>
2024-06-23 17:25:21 +0300

    deploying dev instance

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -66,7 +66,7 @@ export async function POST(req: Request) {
       firstName: first_name!,
       lastName: last_name!,
       photo: image_url,
-    }
+    } 
 
     const newUser = await createUser(user);
 
